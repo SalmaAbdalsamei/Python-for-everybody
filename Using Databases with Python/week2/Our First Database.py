ccec856bf99068e716b53e121940c638d8dc92f1
SalmaAbdalsamei <58674892+SalmaAbdalsamei@users.noreply.github.com>
2020-07-06 05:37:45 +0200

    Update and rename Our First Database to Our First Database.py

---
+++ Using Databases with Python/week2/Our First Database.py
@@ -0,0 +1,32 @@
+#this assignment creates a new Database file and add data to added table
+#then select first raw after convert it's content to hex
+
+
+import sqlite3
+
+#connect to sqlite and create a new file
+con = sqlite3.connect('first')
+cur= con.cursor()
+
+#delete existing data then create new one
+cur.execute('DROP TABLE IF EXISTS Ages')
+cur.execute('''CREATE TABLE Ages (
+  name VARCHAR(128),
+  age INTEGER
+)''')
+
+#  inserts many records at a time
+insertV = [('Maisey', 39),
+             ('Luk', 17),
+             ('Marco', 36),
+            ('Zakaria', 36),]
+cur.executemany('INSERT INTO Ages VALUES (?,?)', insertV)
+con.commit()
+
+sqlstr ='SELECT hex(name || age) AS X FROM Ages ORDER BY X'
+for raw in cur.execute(sqlstr):
+    print(raw)
+    break
+
+cur.close()
+
