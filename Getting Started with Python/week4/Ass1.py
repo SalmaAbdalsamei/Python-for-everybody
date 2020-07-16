2d162fdf4b7ee7f6322708cdcd6b72acd857e2bb
SalmaAbdalsamei <58674892+SalmaAbdalsamei@users.noreply.github.com>
2020-07-16 23:33:49 +0200

    Create Ass1.py

---
+++ Getting Started with Python/week4/Ass1.py
@@ -0,0 +1,11 @@
+'''
+Write a program to prompt the user for hours and rate per hour using input to compute gross pay. 
+Do not worry about error checking or bad user data.
+'''
+# This first line is provided for you
+
+hrs = input("Enter Hours:")
+rate= input ("Enter Rate per hour")
+
+grosspay = float(hrs) * float( rate)
+print("Pay:",grosspay)
