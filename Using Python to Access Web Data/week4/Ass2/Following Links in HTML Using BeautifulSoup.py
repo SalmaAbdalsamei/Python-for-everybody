95af315b5551a51c5fe7433d3516230c6987e99e
SalmaAbdalsamei <58674892+SalmaAbdalsamei@users.noreply.github.com>
2020-07-16 22:26:14 +0200

    Update Following Links in HTML Using BeautifulSoup.py

--- Using Python to Access Web Data/week4/Ass2/Following Links in HTML Using BeautifulSoup.py
+++ Using Python to Access Web Data/week4/Ass2/Following Links in HTML Using BeautifulSoup.py
@@ -1,1 +1,31 @@
+'''
+In this assignment you will write a Python program that expands on 
+https://www.py4e.com/code3/urllinks.py
+The program will use urllib to read the HTML from the data files below, 
+extract the href= values from the anchor tags, 
+scan for a tag that is in a particular position from the top and follow that link,
+repeat the process a number of times, and report the last name you find.
+'''
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = input('Enter Url: ')
+count = int(input("Enter count: "))
+position = int(input("Enter position:"))
+for i in range(count):
+    html = urlopen(url).read()
+    soup = BeautifulSoup(html, "html.parser")
+
+    tags = soup('a')
+    s = []
+    t = []
+    for tag in tags:
+        x = tag.get('href', None)
+        s.append(x)
+        y = tag.text
+        t.append(y)
+
+    print (s[position-1])
+    print (t[position-1])
+    url = s[position-1]
 
