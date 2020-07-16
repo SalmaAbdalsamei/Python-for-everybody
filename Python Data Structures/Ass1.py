bb218a7504626b40162db71cfd288937fd3774b9
SalmaAbdalsamei <58674892+SalmaAbdalsamei@users.noreply.github.com>
2020-07-16 23:08:14 +0200

    Update Ass1.py

--- Python Data Structures/Ass1.py
+++ Python Data Structures/Ass1.py
@@ -1,5 +1,5 @@
 '''
- Write code using find() and string slicing to extract the number at the end of the line below.
+ Write code to extract the number at the end of the line below.
  Convert the extracted value to a floating point number and print it out.
  '''
  text = "X-DSPAM-Confidence:    0.8475";
