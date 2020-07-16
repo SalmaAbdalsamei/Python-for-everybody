6c8bcde78efdf800c2433175e4ecac705f80b2d8
SalmaAbdalsamei <58674892+SalmaAbdalsamei@users.noreply.github.com>
2020-07-16 17:27:06 +0200

    Update Databases and Visualization.py

--- Using Databases with Python/week5/Databases and Visualization.py
+++ Using Databases with Python/week5/Databases and Visualization.py
@@ -1,1 +1,7 @@
-
+#----------------------------------------------------------------------
+#In this assignment I use python3 to create a new Database file 
+#Add a many tables 
+#Insert data to DB tables from a json file
+#connect tables with additional table (many to many relation)
+#get new table by using (join) on many tables 
+#----------------------------------------------------------------------
